391763f14c87b5ab078a25e1a3ec7d86a4f37e98
Kristine Kalinina <kviljeva@gmail.com>
2026-03-23 20:49:28 +0100

    add full auth middleware with validation

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,12 +2,38 @@ import jwt from "jsonwebtoken";
 
 export default function authMiddleware(req, res, next) {
   try {
-    
+    // 1. Получаем header
     const authHeader = req.headers.authorization;
 
-    
+    // 2. Проверяем есть ли он
     if (!authHeader) {
       return res.status(401).json({
         message: "No token provided",
       });
     }
+
+    // 3. Проверяем формат Bearer TOKEN
+    const parts = authHeader.split(" ");
+
+    if (parts.length !== 2 || parts[0] !== "Bearer") {
+      return res.status(401).json({
+        message: "Token format invalid",
+      });
+    }
+
+    const token = parts[1];
+
+    // 4. Проверяем токен
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // 5. Сохраняем пользователя
+    req.user = decoded;
+
+   
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid or expired token",
+    });
+  }
+}
